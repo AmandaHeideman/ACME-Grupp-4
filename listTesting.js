0870ba486cf5b5c06086210e3e9bfc7b3f3b6a33
Harald <Harald.Norlin@yh.nackademin.se>
2020-10-22 13:54:42 +0200

    Pair coded admin CRUD with Özgür

--- listTesting.js
+++ listTesting.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-
+event.preventDefault();
 let btn = document.getElementById("submitBtn");
 
 
@@ -18,6 +18,8 @@ class Events{
         this.event = document.createElement("span");
         this.date = document.createElement("span");
         this.location = document.createElement("span");
+        this.delete = document.createElement("button");
+        this.edit = document.createElement("button");
         
         
     }
@@ -25,27 +27,84 @@ class Events{
     addEvent(event, date, location) {
         let eventList = document.createElement("div");
         let listHead = document.createElement("div");
-        
+        this.delete.innerHTML = "Radera";
+        this.edit.innerHTML = "Uppdatera";
+
+        this.delete.classList.add("listBtn");
+        this.edit.classList.add("listBtn");
      
         this.event.innerHTML = event.value;
+        let newEventName = this.event;
         this.date.innerHTML = date.value;
+        let newEventDate = this.date
         this.location.innerHTML = location.value;
+        let newEventLocation = this.location;
 
-        this.location.classList.add("location");
-        this.date.classList.add("date");
-        this.event.classList.add("event");
+        newEventName.classList.add("event");
+        newEventDate.classList.add("date");
+        newEventLocation.classList.add("location");
         
         eventList.classList.add("eventList");
         listHead.classList.add("listHead");
         
         this.container.appendChild(eventList);
         eventList.appendChild(listHead);
-        listHead.appendChild(this.location);
-        listHead.appendChild(this.date);
-        listHead.appendChild(this.event);
+        listHead.appendChild(newEventName);
+        listHead.appendChild(newEventDate);
+        listHead.appendChild(newEventLocation);
+        this.container.appendChild(this.delete);
+        this.container.appendChild(this.edit);
 
-        console.log(this.location)
+        //console.log(this.location)
+
+        this.delete.addEventListener("click", function (e){
+            eventList.remove();
+        });
+
+        this.edit.addEventListener("click", function(e){
+            let updateEventName = document.createElement("input");
+            updateEventName.placeholder = "Uppdatera event";
+            updateEventName.classList.add("event");
+            eventList.appendChild(updateEventName);
+            let updateEventDate = document.createElement("input");
+            updateEventDate.type = "date";
+            updateEventDate.classList.add("date");
+            eventList.appendChild(updateEventDate);
+            let updateEventLocation = document.createElement("input");
+            updateEventLocation.placeholder = "Uppdatera plats";
+            updateEventLocation.classList.add("location");
+            eventList.appendChild(updateEventLocation);
+            let updateBtn = document.createElement("button");
+            eventList.appendChild(updateBtn);
+            updateBtn.innerHTML = "Update Event";
+
+            updateBtn.addEventListener("click", function(e) {
+                if (updateEventName.value != ""){
+                    newEventName.innerHTML = updateEventName.value;
+                }
+
+                if(updateEventDate.value != ""){
+                    newEventDate.innerHTML = updateEventDate.value;
+                }
+                if(updateEventLocation.value != ""){
+                    newEventLocation.innerHTML = updateEventLocation.value;
+                }
+                    updateEventName.remove();
+                    updateEventDate.remove();
+                    updateEventLocation.remove();
+                    updateBtn.remove();
+            })
+            // let updateBtn = document.createElement("button");
+            //console.log(this.event.innerHTML);
+            //this.event = document.createElement("input");
+            //eventList.appendChild(eventEdit);
+
+        });
     }
+
+    // deleteEvent() {
+    //     delete this.container;
+    // }
 }
 
 class Test {
